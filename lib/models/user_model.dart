d7bb96fcb87cc42ef12be08e9160eec85360c1b6
wael <wael.abdallah@etudiant-enit.utm.tn>
2024-03-16 11:45:38 +0100

    V0

---
+++ lib/models/user_model.dart
@@ -0,0 +1,23 @@
+class UserModel {
+  final int id;
+  final String email;
+  final String firstname;
+  final String lastname;
+  final String avatar;
+
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.firstname,
+    required this.lastname,
+    required this.avatar,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json){
+    return UserModel(id: json['id'],
+        email: json['email'],
+        firstname: json['first_name'] ?? "first name ",
+        lastname: json['last_name'] ?? 'last name ',
+        avatar: json['avatar'] ?? 'hhhh' );
+  }
+}
