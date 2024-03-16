4099638f0b71a3df1f701c0812a5838a8ace6b87
wael <wael.abdallah@etudiant-enit.utm.tn>
2024-03-16 12:28:22 +0100

    fixing dependencies

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:dio_json/models/user_model.dart';
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key,required this.e});
+  final UserModel e;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
